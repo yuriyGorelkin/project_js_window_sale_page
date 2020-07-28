bcfe400bfa4317ce683fa943f9a08d2fb3eb2be4
Yurii <yuriygorelkin@gmail.com>
2020-07-28 09:13:47 +0300

    refactored function modals and added function showModalByTime

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,31 +1,47 @@
 const modals = () => {
-    function bindModals(trigger, modal, close) {
-        trigger.addEventListener('click', (e) => {
-            if (e.target) {
-                e.preventDefault
-            }
-            modal.style.display = 'block'
-            document.body.classList.add = 'modal-open'
+    function bindModals(triggerSelector, modalSelector, closeSelector) {
+
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector)
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault()
+                }
+
+                modal.style.display = 'block'
+                document.body.style.overflow = 'hidden'
+                // document.body.classList.add = 'modal-open'
+            })
         })
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
                 modal.style.display = 'none'
-                document.body.classList.remove = 'modal-open'
+                document.body.style.overflow = ''
+                // document.body.classList.remove = 'modal-open'
             }
         })
 
         close.addEventListener('click', () => {
             modal.style.display = 'none'
-            document.body.classList.remove = 'modal-open'
+            document.body.style.overflow = ''
+            // document.body.classList.remove = 'modal-open'
         })
     }
 
-    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-        modalEngineer = document.querySelector('.popup_engineer'),
-        modalEngineerClose = document.querySelector('.popup_engineer .popup_close')
+    function showModalByTime(selector, time) {
+        setTimeout(() => {
+            document.querySelector(selector).style.display = 'block'
+            document.body.style.overflow = 'hidden'
+        }, time)
+    }
 
-    bindModals(callEngineerBtn, modalEngineer, modalEngineerClose)
+    bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
+    bindModals('.phone_link', '.popup', '.popup .popup_close')
+    showModalByTime('.popup', 6000)
 }
 
 export default modals
