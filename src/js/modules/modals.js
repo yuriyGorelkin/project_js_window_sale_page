1f0816db0f40fe841a3607aec6f9d2f23f03e80b
Yurii <yuriygorelkin@gmail.com>
2020-08-10 17:05:45 +0300

    added functional of change popups in calc form

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,9 +1,10 @@
 const modals = () => {
-    function bindModals(triggerSelector, modalSelector, closeSelector) {
+    function bindModals(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector)
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]')
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -11,24 +12,37 @@ const modals = () => {
                     e.preventDefault()
                 }
 
+                windows.forEach(item => {
+                    item.style.display = 'none'
+                })
+
                 modal.style.display = 'block'
                 document.body.style.overflow = 'hidden'
                 // document.body.classList.add = 'modal-open'
             })
         })
 
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none'
+            })
+
+            modal.style.display = 'none'
+            document.body.style.overflow = ''
+            // document.body.classList.remove = 'modal-open'
+        })
+
+
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach(item => {
+                    item.style.display = 'none'
+                })
+
                 modal.style.display = 'none'
                 document.body.style.overflow = ''
                 // document.body.classList.remove = 'modal-open'
             }
-        })
-
-        close.addEventListener('click', () => {
-            modal.style.display = 'none'
-            document.body.style.overflow = ''
-            // document.body.classList.remove = 'modal-open'
         })
     }
 
@@ -41,6 +55,9 @@ const modals = () => {
 
     bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
     bindModals('.phone_link', '.popup', '.popup .popup_close')
+    bindModals('.popup_calc_btn', '.popup_calc', '.popup_calc_close')
+    bindModals('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false)
+    bindModals('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false)
     // showModalByTime('.popup', 60000)
 }
 
